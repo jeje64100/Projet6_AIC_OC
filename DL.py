85cd7cc370ccf062289be89cc43b6324c981f28a
Jérôme Martiquet <jeromemartiquet@hotmail.fr>
2021-04-26 19:28:44 +0200

    transfo script en module

--- DL.py
+++ DL.py
@@ -1,11 +1,14 @@
 from tqdm import tqdm
 import requests
 
-url = "https://github.com/glpi-project/glpi/releases/download/9.3.3/glpi-9.3.3.tgz"
-response = requests.get(url, stream=True)
 
-with open("glpi.tar", "wb") as fichier:
-	for data in tqdm(response.iter_content()):
-		fichier.write(data)
+def telechargement(adresse, nom):
+	url=adresse
+	response = requests.get(url, stream=True)
 
-fichier.close()
+	with open(nom, "wb") as fichier:
+		for data in tqdm(response.iter_content()):
+			fichier.write(data)
+	fichier.close()
+
+
