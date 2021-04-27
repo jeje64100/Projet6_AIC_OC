5b4093b8fa84f263866f2adc7b308698bb16f85d
Jérôme Martiquet <jeromemartiquet@hotmail.fr>
2021-04-27 19:13:58 +0200

    rajout parametre installation sans interaction GLPI + correction du chemin/bin/console pour le check des paquets

--- scriptinstallglpi.py
+++ scriptinstallglpi.py
@@ -1,5 +1,5 @@
 import subprocess
 
 def install(chemin, langue, hote, base, user, mdp):
-	subprocess.run(["php", "bin/console", "glpi:system:check_requirements"])
-	subprocess.run(["php", chemin+"/glpi/bin/console", "db:install", "--force", "--default-language="+langue, "--db-host="+hote, "--db-name="+base, "--db-user="+user, "--db-password="+mdp])
+	subprocess.run(["php", chemin+"/glpi/bin/console", "glpi:system:check_requirements"])
+	subprocess.run(["php", chemin+"/glpi/bin/console", "db:install", "--no-interaction", "--default-language="+langue, "--db-host="+hote, "--db-name="+base, "--db-user="+user, "--db-password="+mdp])
