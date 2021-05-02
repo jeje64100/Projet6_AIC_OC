35b8660a6f25d52c8d078441364bcf7d59ebf2d9
Jérôme Martiquet <jeromemartiquet@hotmail.fr>
2021-05-02 15:46:29 +0200

    rajout dans le module d'installation de fusioninventory de la création d'un crontab pour le plugin

--- scriptinstallfi.py
+++ scriptinstallfi.py
@@ -1,5 +1,13 @@
 import subprocess
+from crontab import CronTab
 
 def installfi(chemin):
 	subprocess.run(["php", chemin+"/glpi/bin/console", "glpi:plugin:install", "--username", "glpi", "fusioninventory"])
 	subprocess.run(["php", chemin+"/glpi/bin/console", "glpi:plugin:activate", "fusioninventory"])
+
+	cron=CronTab(user='www-data')
+	job=cron.new(command='/usr/bin/php5 '+chemin+'/glpi/front/cron.php &>/dev/null')
+	job.minute.every(1)
+	cron.write()
+
+	subprocess.run(["systemctl", "restart", "cron"])
