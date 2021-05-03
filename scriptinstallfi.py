3d053b99611f348e81772dac8944fd5c74842556
Jérôme Martiquet <jeromemartiquet@hotmail.fr>
2021-05-03 11:29:04 +0200

    rajout d'un bloc try/except pour la création du crontab dans le module d'installation de fusioninventory

--- scriptinstallfi.py
+++ scriptinstallfi.py
@@ -4,10 +4,14 @@ from crontab import CronTab
 def installfi(chemin):
 	subprocess.run(["php", chemin+"/glpi/bin/console", "glpi:plugin:install", "--username", "glpi", "fusioninventory"])
 	subprocess.run(["php", chemin+"/glpi/bin/console", "glpi:plugin:activate", "fusioninventory"])
+	try:
+		cron=CronTab(user='www-data')
+		job=cron.new(command='/usr/bin/php5 '+chemin+'/glpi/front/cron.php &>/dev/null')
+		job.minute.every(1)
+		cron.write()
 
-	cron=CronTab(user='www-data')
-	job=cron.new(command='/usr/bin/php5 '+chemin+'/glpi/front/cron.php &>/dev/null')
-	job.minute.every(1)
-	cron.write()
+	except TypeError:
+		print("Le mauvais module est installé.Merci de désinstaller crontab et d'installer python-crontab")
+		return exit()
 
 	subprocess.run(["systemctl", "restart", "cron"])
