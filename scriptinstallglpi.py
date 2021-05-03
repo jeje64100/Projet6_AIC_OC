f572067ec61c8e83e0d86525540e0034a607dff3
Jérôme Martiquet <jeromemartiquet@hotmail.fr>
2021-05-03 12:26:01 +0200

    rajout dans le module d'installation de glpi, la suppression après l'installation du fichier install.php

--- scriptinstallglpi.py
+++ scriptinstallglpi.py
@@ -3,3 +3,5 @@ import subprocess
 def install(chemin, langue, hote, base, user, mdp):
 	subprocess.run(["php", chemin+"/glpi/bin/console", "glpi:system:check_requirements"])
 	subprocess.run(["php", chemin+"/glpi/bin/console", "db:install", "--no-interaction", "--default-language="+langue, "--db-host="+hote, "--db-name="+base, "--db-user="+user, "--db-password="+mdp])
+	subprocess.run(["rm", chemin+"/glpi/install/install.php"])
+
