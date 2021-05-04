b70ee3a787b97ebcb1d17d666411355d0dd50c23
Jérôme Martiquet <jeromemartiquet@hotmail.fr>
2021-05-04 21:24:40 +0200

    modification du module droits.py: utilisation de os.chown au lieu de subprocess + rajout du bloc try/except

--- droits.py
+++ droits.py
@@ -1,4 +1,21 @@
-import subprocess
+import os
 
-def droits(path):
-	subprocess.run(["chown", "-R", "www-data:www-data", path+"/glpi/"])
+
+def droits(chemin, uid, gid):
+
+	try:
+		path=chemin+"/glpi/"
+		for root, dirs, files in os.walk(path):
+			for d in dirs:
+				os.chown(os.path.join(root, d), uid, gid)
+			for f in files:
+				os.chown(os.path.join(root, f), uid, gid)
+
+	except PermissionError:
+		print("Vous n'avez pas les droits nécesssaires, veuillez passer en root ou super-utilisateur")
+		return exit()
+
+	except TypeError:
+		print("L'UID et le GID doivent être des entiers")
+		return exit()
+
