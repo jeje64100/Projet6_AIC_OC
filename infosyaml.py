7f8a022f3983227ed4308623af1133e5c96f0432
Jérôme Martiquet <jeromemartiquet@hotmail.fr>
2021-05-06 20:17:17 +0200

    rajout exit() dans l'instruction except du module infosyaml.py

--- infosyaml.py
+++ infosyaml.py
@@ -5,5 +5,7 @@ def Fyaml(fichier):
 		with open(fichier, "r") as donnees:
 			donnees_content=yaml.load(donnees, Loader=yaml.BaseLoader)
 		return donnees_content
+
 	except FileNotFoundError:
 		print("Le fichier",fichier,"n'existe pas")
+		return exit()
